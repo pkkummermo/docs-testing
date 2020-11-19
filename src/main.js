baa70fc3c79febecc1802b984a8b8cf34c7c2341
Per Kristian Kummermo <per.kristian@telenordigital.com>
2020-11-19 19:12:43 +0100

    fix(tailwind): centralize tailwind global config

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 // Import tailwind
-require("./tailwind.css");
+require("./tailwind.scss");
 
 // This is the main.js file. Import global CSS and scripts here.
 // The Client API can be used here. Learn more: gridsome.org/docs/client-api
